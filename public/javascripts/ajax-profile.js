41ef4ec24b279bbb69810e6be780423d37fe3196
Safiya Cain <say.cain3@gmail.com>
2020-11-06 18:41:43 -0500

    dom manipulation

--- public/javascripts/ajax-profile.js
+++ public/javascripts/ajax-profile.js
@@ -1,6 +1,7 @@
 window.addEventListener('DOMContentLoaded', ()=> {
-
-    const avatar = document.querySelector('.avatar');
-
-    avatar.src = 
+    const img = document.querySelector('.avatar');
+    const avatar = document.getElementById('avatar');
+    avatar.addEventListener('click', (e)=> {
+        img.setAttribute('src', 'https://media.giphy.com/media/4oMoIbIQrvCjm/giphy.gif');
+    })
 })
