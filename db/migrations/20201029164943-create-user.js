454564bcbeac4694796ff4a08257af217cd96445
Hassan Tariq <66930201+hassanmt96@users.noreply.github.com>
2020-11-07 16:48:21 -0600

    slight error fixed

--- db/migrations/20201029164943-create-user.js
+++ db/migrations/20201029164943-create-user.js
@@ -27,10 +27,7 @@ module.exports = {
 				type: Sequelize.STRING.BINARY,
 				allowNull: false,
 			},
-			image: {
-				type: Sequelize.TEXT,
-				allowNull: false,
-			},
+	
 			createdAt: {
 				allowNull: false,
 				type: Sequelize.DATE,
